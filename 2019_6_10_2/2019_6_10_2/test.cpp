65b535eda1474698193addbc6c57da1498dc5cea
Asupi <1099345943@qq.com>
2019-06-10 17:08:42 +0800

    哈希（开散列，闭散列）

---
+++ 2019_6_10_2/2019_6_10_2/test.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include"hash.h"
+
+#include<iostream>
+//void testHash()
+//{
+//	HashTable<int, int> ht;
+//	ht.Insert(make_pair(1, 1));
+//	ht.Insert(make_pair(2, 2));
+//	ht.Insert(make_pair(3, 3));
+//	ht.Insert(make_pair(4, 4));
+//	ht.Insert(make_pair(5, 5));
+//	ht.Insert(make_pair(6, 6));
+//
+//	auto ret = ht.Find(6);
+//	ret = ht.Find(10);
+//}
+
+int main()
+{
+	//testHash();
+	
+	return 0;
+}
