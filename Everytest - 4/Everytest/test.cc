e1288118ab9a38069f1ce9d64b50644afe733668
Asupi <1099345943@qq.com>
2019-07-19 22:55:04 +0800

    抄送列表（笔试题）

---
+++ Everytest - 4/Everytest/test.cc
@@ -0,0 +1,51 @@
+链接：https://www.nowcoder.com/questionTerminal/286af664b17243deb745f69138f8a800
+来源：牛客网
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s, res;
+    while (getline(cin, s))
+    {
+        getline(cin, res);
+        bool sta = false;
+        for (int i = 0; i < s.length(); i++)
+        {
+            string t;
+            if (s[i] == '"')
+            {
+                i++;
+                while (i<s.length()&&s[i] != '"')
+                {
+                    t += s[i];
+                    i++;
+                }
+                if (t == res)
+                {
+                    sta = true;
+                    break;
+                }
+            }
+            else if (s[i] != ',')
+            {
+                while (i<s.length()&&s[i] != ',')
+                {
+                    t += s[i];
+                    i++;
+                }
+                if (t == res)
+                {
+                    sta = true;
+                    break;
+                }
+            }
+        }
+        if (sta == true)
+            cout << "Ignore" << endl;
+        else
+            cout << "Important!" << endl;
+    }
+    return 0;
+}
